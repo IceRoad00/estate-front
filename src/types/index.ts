714dffa409fb7e47cadea4f897c146f154c827a2
IceyoungK <anxis001220@gmail.com>
2024-06-04 16:49:31 +0900

    first

--- src/types/index.ts
+++
@@ -1,8 +0,0 @@
-export interface BoardListItem {
-    receptionNumber: number;
-    status: boolean;
-    title: string;
-    writerId: string;
-    writeDatetime: string;
-    viewCount: number;
-}
