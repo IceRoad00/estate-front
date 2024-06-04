9871dd0ce103c0af934e02e9cd41bbd043002a8a
IceyoungK <anxis001220@gmail.com>
2024-06-04 16:50:08 +0900

    first

---
+++ src/components/Inputbox/index.tsx
@@ -0,0 +1,46 @@
+import React, { ChangeEvent, KeyboardEvent } from "react";
+import './style.css';
+
+export interface InputBoxProps {
+    label: string;
+    type: 'text' | 'password' | 'email' | 'number';
+    value: string;
+    placeholder: string;
+    onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
+    buttonTitle?: string;
+    ButtonStatus?: boolean;
+    onButtonClickHandler?: () => void;
+    message?: string;
+    error? : boolean;
+    onKeydownHandler?: (event: KeyboardEvent<HTMLInputElement>) => void;
+}
+
+export default function InputBox({label, type, value, placeholder, onChangeHandler, buttonTitle, ButtonStatus, onButtonClickHandler, message, error, onKeydownHandler}: InputBoxProps) {
+
+    const buttonClass = ButtonStatus ? 'input-primary-button' : 'input-disable-button';
+    const messageClass = 'input-message ' + (error ? 'error' : 'primary');
+
+    return (
+        <div className="input-box">
+            <div className="input-label label">{label}</div>
+            <div className="input-content-box">
+                <input
+                    className="input"
+                    type={type}
+                    value={value}
+                    placeholder={placeholder}
+                    onChange={onChangeHandler}
+                    onKeyDown={onKeydownHandler}
+                />
+                {buttonTitle && 
+                <div className={buttonClass} onClick={onButtonClickHandler}>
+                    {buttonTitle}
+                </div> 
+                }
+            </div>
+            <div className={messageClass}>
+                {message}
+            </div>
+        </div>
+    );
+}
